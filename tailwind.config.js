b27ba875a84379ec8bc05e8ef5ef13be59805678
Sebastian Anioke <aniokechukwudi7@gmail.com>
2022-08-14 03:21:19 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+  ],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
